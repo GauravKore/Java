30812fd45a3c29b11f9deae941f60ad980270cd2
Gaurav <gauravkore50@gmail.com>
2026-08-10 09:22:47 +0530

    Multithreading in progress

---
+++ Java Core/Multithreading/demo3.java
@@ -0,0 +1,14 @@
+public class demo3 {
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName());
+        System.out.println(Thread.currentThread().getId());
+    
+
+    Thread t1 = new Thread(() -> {
+        System.out.println("Name of my thread is "+ Thread.currentThread().getName());
+        System.out.println("Id of my thread is " + Thread.currentThread().getName());
+    });
+
+    t1.start();
+   }
+}
