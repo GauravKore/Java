951bf790519c88eab896c9c8ddc1f5d6e10d2ec5
Gaurav <gauravkore50@gmail.com>
2026-08-11 23:02:15 +0530

    Multithreading in progress

---
+++ Java Core/Multithreading/ExecutionOrder.java
@@ -0,0 +1,25 @@
+public class ExecutionOrder {
+    public static void main(String[] args) {
+        // 1 to 100 (even numbers)
+        Thread t1 = new Thread(() -> {
+            for(int i = 1 ; i <=100 ; i++){
+                if(i % 2 == 0){
+                    System.out.println("T1 :" + i);
+                }
+            }
+        });
+
+        // 1 to 100 (odd numbers)
+        Thread t2 = new Thread(() -> {
+            for(int i = 1 ; i <=100 ; i++){
+                if(i % 2 != 0){
+                    System.out.println("T2 :" + i);
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+    
+}
